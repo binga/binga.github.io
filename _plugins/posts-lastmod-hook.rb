604f464af855905278feda1a14e41ecd42951a3c
binga <phanisrikanth33@gmail.com>
2026-09-08 02:21:33 +0530

    .

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -1,14 +1,43 @@
 #!/usr/bin/env ruby
 #
-# Check for changed posts
+# Check for meaningful changes to posts. Taxonomy-only edits should not make an
+# old article appear in the "Recently Updated" list.
+
+require 'open3'
+
+def normalized_post_content(content)
+  content.sub(/\A---\s*\n(.*?)\n---\s*\n/m) do
+    front_matter = Regexp.last_match(1)
+    front_matter = front_matter.lines.reject do |line|
+      line.match?(/\A(?:categories|tags):/)
+    end.join
+
+    "---\n#{front_matter}---\n"
+  end
+end
+
+def git_output(*args)
+  output, status = Open3.capture2('git', *args)
+  status.success? ? output : nil
+end
 
 Jekyll::Hooks.register :posts, :post_init do |post|
+  path = post.relative_path.sub(%r{\A/}, '')
+  history = git_output('log', '--format=%H', '--', path)
+  next unless history
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  commits = history.lines.map(&:strip)
+  next if commits.length < 2
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
-  end
+  meaningful_commit = commits.each_cons(2).find do |current_commit, previous_commit|
+    current = git_output('show', "#{current_commit}:#{path}")
+    previous = git_output('show', "#{previous_commit}:#{path}")
 
+    current && previous && normalized_post_content(current) != normalized_post_content(previous)
+  end&.first
+
+  next unless meaningful_commit
+
+  lastmod_date = git_output('show', '-s', '--format=%aI', meaningful_commit)
+  post.data['last_modified_at'] = lastmod_date.strip if lastmod_date
 end
